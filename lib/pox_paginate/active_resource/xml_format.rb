c688a50acf986be873666697adcca07203ec9d6e
klacointe <kevinlacointe@gmail.com>
2010-11-24 17:25:41 +0800

    create empty RemoteCollection on blank xml response

--- lib/pox_paginate/active_resource/xml_format.rb
+++ lib/pox_paginate/active_resource/xml_format.rb
@@ -4,14 +4,19 @@ module PoxPaginate
       def self.included(mod)
         mod.module_eval do
           def decode_with_pagination_support(xml)
-            deserialised_xml = decode_without_pagination_support(xml)
-            root_attributes = ::ActiveSupport::XmlMini.root_node_attributes(xml)
-            if root_attributes['type'] == "array" && root_attributes['current_page']
-              RemoteCollection.create(root_attributes['current_page'], root_attributes['per_page'], root_attributes['total_entries']) do |pager|
-                pager.replace deserialised_xml
+            unless xml.blank?
+              deserialised_xml = decode_without_pagination_support(xml)
+              root_attributes = ::ActiveSupport::XmlMini.root_node_attributes(xml)
+              if root_attributes['type'] == "array" && root_attributes['current_page']
+                RemoteCollection.create(root_attributes['current_page'], root_attributes['per_page'], root_attributes['total_entries']) do |pager|
+                  pager.replace deserialised_xml
+                end
+              else
+                deserialised_xml
               end
             else
-              deserialised_xml
+              RemoteCollection.create(1, 1, 0) do |pager|
+              end
             end
           end
           alias_method_chain :decode, :pagination_support
